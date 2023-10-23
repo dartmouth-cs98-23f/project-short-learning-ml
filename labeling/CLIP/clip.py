f41da9c62ef2cf9f04453c07d0cd78330ede2fdf
siavava <amittaijoel@outlook.com>
2023-10-22 21:09:23 -0400

    update model, tests

---
+++ labeling/CLIP/clip.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+  CLIP Model for Labeling.
+
+  This wrapper around the CLIP model exposes a more friendly API
+  for interacting with the model and auto-downloading the necessary.
+"""
+
+from typing import List
+
+from PIL import Image
+import numpy as np
+from transformers import CLIPProcessor, CLIPModel
+
+import utils
+
+class Model():
+
+  def __init__(self):
+    self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    
+    self.model.config.add_cross_attention = True
+  
+    self.decodes = 0
+    self.trim_frames = True
+
+    self.categories = [
+          "mathematics"
+        , "technology"
+        , "sports"
+        , "computer science"
+        , "physics"
+        , "geography"
+        , "biology"
+        , "chemistry"
+        , "history"
+        , "art"
+        , "economics"
+        , "politics"
+        , "music"
+        , "literature"
+        , "philosophy"
+        , "engineering"
+        , "medicine"
+        , "psychology"
+        , "sociology"
+        , "astronomy"
+        , "linguistics"
+        , "anthropology"
+        , "law"
+      ]
+    
+  
+    self.model.config.num_labels = len(self.categories)
+
+
+
+  def __call__(self, labels: List[str], image: Image.Image) -> float:
+    """
+      Returns the similarity score between the text and image.
+
+      Args:
+        - `text (str)`: text to compare
+        - `image (Image.Image)`: image to compare
+
+      Returns:
+        - `float`: similarity score
+    """
+
+    inputs = self.processor(text=labels, images=image, return_tensors="pt", padding=True)
+    outputs = self.model(**inputs)
+    logits_per_image = outputs.logits_per_image
+    probs = logits_per_image.softmax(dim=1).detach().numpy()
+
+    self.decodes += 1
+    if self.decodes % 10 == 0:
+      print(f"Decodes: {self.decodes}")
+    return probs
+  
+  def process(self, id: str):
+
+    data = utils.load_data(id)
+
+    if self.trim_frames: data = data[:120] # focus on first 2 minutes
+
+    runner = lambda x: self(self.categories, x)[0]
+
+    extract_max = lambda x: [self.categories[i] for i in np.argsort(x)[-3:][::-1]]
+
+    softmax_values = data["image"].apply(runner)
+
+    # set category to the top 3 softmax values
+    data["categories"] = softmax_values.apply(extract_max)
+
+    # this is not needed, we can eventually drop it.
+    # I just wanted to visualize the actual softmax values
+    for i in range(len(self.categories)):
+      category = self.categories[i]
+      data[category] = softmax_values.apply(lambda x: x[i])
+
+    # save to csv the results (minus the image column)
+    data = data.drop(columns=["image"])
+    data.to_csv(f"data/output/{id}.csv")
+
+
+if __name__ == "__main__":
+  model = Model()
+
+  # maybe do something more interesting?
--- labeling/CLIP/utils.py
+++ labeling/CLIP/utils.py
@@ -9,27 +9,32 @@ import shutil
 import urllib3
 import os
 from pytube import YouTube
+from youtube_transcript_api import YouTubeTranscriptApi
+
+import pandas as pd
+from pandas import DataFrame
 
 import cv2
 from cv2 import VideoCapture, imwrite
 
-import math
+from typing import Optional
+from PIL import Image
 
-def download_file(url: str, filename: str) -> None:
+def download_video(url: str, filename: str) -> None:
   """
-  Download a file from url to filename
+    Download a video from url to outfile.
 
-  Args:
-    - `url (str)` : url to download from  
-    - `filename (str)`: filename to save to
+    Args:
+      - `url (str)` : url of video
+      - `outfile (str)`: filename to save to
 
-  Returns:
-    - `None`
+    Returns:
+      - `None`
 
-  Raises an exception: if error connecting or downloading
+    Raises an exception: if error connecting or downloading
   """
   if os.path.exists(filename):
-    print(f"{filename} already exists")
+    print(f"Video {filename} already downloaded")
     return
 
   try:
@@ -44,6 +49,26 @@ def download_file(url: str, filename: str) -> None:
   try:
     stream.download(filename=filename)
     print(f"Downloaded {filename} from {url}")
+    return
+  
+  except Exception as e:
+    print(f"Error Downloading: {e}")
+    raise e
+  
+def download_transcript(id: str, filename: str) -> None:
+  try:
+    transcript = YouTubeTranscriptApi.get_transcript(id)
+    with open(filename, "w") as f:
+      f.write("start,duration,text\n")
+      for line in transcript:
+        # print(f"{line['start']} {line['duration']} {line['text']}")
+
+        stripped = line['text'].strip(" \n'\"")
+        stripped = stripped.replace('\n', ' ')
+
+        f.write(f"{line['start']},{line['duration']},\"{stripped}\"\n")
+    
+    print(f"Downloaded transcript for {id} to {filename}")
     return
   
   except Exception as e:
@@ -65,12 +90,9 @@ def extract_frames(file: str, save_dir: str) -> None:
   """
   
   # if save_dir exists, delete it
-  # and recreate it.
-
   if os.path.exists(save_dir):
     shutil.rmtree(save_dir)
 
-  
   try:
     os.makedirs(save_dir)
 
@@ -85,31 +107,124 @@ def extract_frames(file: str, save_dir: str) -> None:
 
   # get video fps
   fps = round(capture.get(cv2.CAP_PROP_FPS))
-  print(f"FPS: {fps}")
+  print(f"Video {file} has FPS: {fps}")
 
   success, image = capture.read()
   count = 0
 
   while success:
-
-
+    
     if count % fps == 0:
       imwrite(os.path.join(save_dir, f"{count // fps}.jpg"), image)
+    
     success, image = capture.read()
     count += 1
 
+  print(f"{count} frames extracted from {file}")
+
   return
 
+def process_video(id: str) -> None:
+  """
+  Download a youtube video and extract frames and transcript.
 
-    
+  Args:
+    - `id (str)` : video id to download
+
+  Returns:
+    - `None`
+
+  Raises an exception: if error connecting or downloading
+  """
+
+  url = f"https://www.youtube.com/watch?v={id}"
+
+  # download video
+  download_video(url, f"data/videos/{id}.mp4")
+
+  """
+    Transcript stuff, ignore for now.
+  """
+  # download_transcript(id, f"data/transcripts/{id}.csv")
+
+  # extract frames
+  extract_frames(f"data/videos/{id}.mp4", f"data/frames/{id}")
+
+  return
+
+def load_datapoint(id: str, frame: int) -> Optional[dict]:
+  """
+  Load a *single*datapoint from the dataset.
+
+  Args:
+    - `id (str)` : video id
+    - `frame (int)`: frame number
+
+  Returns:
+    - `Optional[dict]`: datapoint if it exists, else None
+  """
+
+  # check if frame exists
+  frame_path = f"data/frames/{id}/{frame}.jpg"
+  if not os.path.exists(frame_path):
+    return None
+  
+  """
+    Transcript stuff, ignore for now.
+  """
+  # transcript_values = transcript[transcript['start'].between(frame - 5, frame + 5)]
+  # if transcript_values.empty:
+  #   text = ""
+  # else:
+  #   # concatenate the results
+  #   text = " ".join(transcript_values['text'].values)
+
+  return {
+    "frame": frame,
+    "image": Image.open(frame_path),
+  }
+
+def load_data(id: str) -> DataFrame:
+  """
+  Load the data for a video id.
+
+  Args:
+    - `id (str)` : video id
+
+  Returns:
+    - `DataFrame`: dataframe of data
+  """
+
+  # if frames directory does not exist, process video to download and extract frames
+  if not os.path.exists(f"data/frames/{id}"):
+    process_video(id)
+
+  frames = os.listdir(f"data/frames/{id}")
+
+  datapoints = [ load_datapoint(id, i) for i in range(len(frames)) ]
+  filter(lambda x: x is not None, datapoints)
+  
+  df = DataFrame(datapoints)
+  df.set_index("frame", inplace=True)
+  return df
+
 
 if __name__ == "__main__":
 
   # test download_file
-  url = "https://www.youtube.com/watch?v=_XdD-TQseU4"
-  filename = "test.mp4"
+  # url = "https://www.youtube.com/watch?v=_XdD-TQseU4"
+  id = "_XdD-TQseU4"
+  # filename = "test.mp4"
 
-  download_file(url, filename)
+  # process_video(id)
 
-  extract_frames(filename, "data/test_frames")
+  # extract_frames(filename, "data/test_frames")
+
+  # transcript_df = pd.read_csv(f"data/transcripts/{id}.csv", sep=',', encoding="utf-8")
+  # print(transcript_df.head(30))
+
+  # test load_datapoint
+  # datapoint = load_datapoint(id, 20, transcript_df)
+  data = load_data(id)
+  print(f"{data.head(10)}")
 
