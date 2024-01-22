30fcc03640071926ccd223f48ba73ff607d460b3
siavava <amittaijoel@outlook.com>
2024-01-22 00:15:42 -0500

    add docstring

--- app/utils/smoothing.py
+++ app/utils/smoothing.py
@@ -8,6 +8,59 @@
 import pandas as pd
 
 def tfidf(df: pd.DataFrame, /, start_column=0) -> pd.DataFrame:
+  """
+    TDIDF (Term Frequency Inverse Document Frequency) is often used as a way
+    of mitigating the effect of common words in a corpus.
+    
+    For example, if we have a corpus of 100 documents, and the word "the" appears
+    in 99 of them, then the word "the" is not very useful for distinguishing
+    between documents.
+    
+    Since we want to use keywords in specific subdomains of computer science,
+    it is likely that certain keywords will appear across many time periods in a video
+    (for example, "JOIN" or "SELECT" in SQL).
+    We don't want to throw such keywords out entirely since they are relevant to the subject,
+    but we need a way to measure how "useful" a keyword's presence in a specific time period is.
+    
+    This function computes tf-idf for a given input dataframe.
+    
+    This does the following:
+    
+    1. Divides each cell by the sum of the column (if nonzero),
+       so values that appear across all rows are weighted less.
+       it ignores the rows before the start column (int index),
+       if provided.
+       
+    2. Normalizes so each row adds up to 1,
+       so values do not vanish or explode.
+       
+    Parameters
+    ----------
+    df: pd.DataFrame
+      The dataframe to compute tf-idf on.
+      NOTE: We create a copy of this dataframe, so the original is not modified.
+      
+    start_column: int (optional)
+      The column (index) to start tf-idf on.
+      This is useful to avoid computing on the first few columns which may be row labels.
+      
+    Returns
+    -------
+    A new dataframe with tf-idf applied.
+    
+    Example
+    -------
+    >>> df = pd.DataFrame([
+    ...   [1, 1, 1],
+    ...   [0, 0, 0],
+    ...   [3, 2, 0]
+    ... ], columns=["a", "b", "c"])
+    >>> tfidf(df) # notice how the first column is re-weighted based on uniqueness
+    ...             a         b         c
+    ...   0  0.157895  0.210526  0.631579
+    ...   1  0.000000  0.000000  0.000000
+    ...   2  0.529412  0.470588  0.000000
+  """
   
   #? copy everything after the start column, inclusive
   df = df.iloc[:, start_column:].copy()
